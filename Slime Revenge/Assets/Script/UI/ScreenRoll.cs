c5edb74a1ca4feddedb94fa90d2099ebe909600f
pc\korchat <korchat@hotmail.com>
2017-01-16 22:34:19 +0700

    no message

--- Slime Revenge/Assets/Script/UI/ScreenRoll.cs
+++ Slime Revenge/Assets/Script/UI/ScreenRoll.cs
@@ -5,15 +5,17 @@ using UnityEngine.UI;
 public class ScreenRoll : MonoBehaviour
 {
     private float speed=1f;
+    public float sensitive=7f;
+    private int maxscene = 4;
     private bool mouseTouch = false;
     private int movePattern=0;
-    public Vector2 startPos;
+    private Vector2 startPos;
     private Vector2 mousePos;
     private Vector2 prevMousePos;
-    private Vector3[] screenPos = new Vector3[3];
+    private Vector3[] screenPos = new Vector3[4];
     // Use this for initialization
     void Start()
-    {   for (int i = 0; i < 3; i++)
+    {   for (int i = 0; i < maxscene; i++)
         {
             screenPos[i] = this.transform.GetChild(i).transform.position;
         }
@@ -26,17 +28,17 @@ public class ScreenRoll : MonoBehaviour
         else
             movePattern--;
 
-        if (movePattern > 2) movePattern = 0;
-            else if (movePattern < 0) movePattern = 2;
+        if (movePattern > maxscene-1) movePattern = 0;
+            else if (movePattern < 0) movePattern = maxscene - 1;
         ResetPos();
 
     }
 
     public void ResetPos()
     {
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < maxscene; i++)
         {
-            this.transform.GetChild(i).transform.position = screenPos[(i + movePattern) % 3];
+            this.transform.GetChild(i).transform.position = screenPos[(i + movePattern) % maxscene];
         }
 
     }
@@ -58,27 +60,29 @@ public class ScreenRoll : MonoBehaviour
         if (Input.GetMouseButton(0) && mouseTouch)
         {
             mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            for (int i = 0; i < 3; i++) {
+            for (int i = 0; i < maxscene; i++) {
                 Vector3 pos = this.transform.GetChild(i).transform.position;
                 this.transform.GetChild(i).Translate((mousePos.x - prevMousePos.x)*speed,0,0);
             }
-            if (mousePos.x > startPos.x + 7f)
-            {
-               
-                RotateScreen(true);
-                startPos = mousePos;
-            }
-            if (mousePos.x < startPos.x -7f)
-            {
-             
-                RotateScreen(false);
-                startPos = mousePos;
-            }
+          
             prevMousePos = mousePos;
         }
         if (Input.GetMouseButtonUp(0) && mouseTouch)
         {
-            ResetPos(); mouseTouch = false;
+            if (mousePos.x > startPos.x + sensitive)
+            {
+
+                RotateScreen(true);
+                startPos = mousePos;
+            }
+            if (mousePos.x < startPos.x - sensitive)
+            {
+
+                RotateScreen(false);
+                startPos = mousePos;
+            }
+            ResetPos();
+            mouseTouch = false;
         }
     }
 }
