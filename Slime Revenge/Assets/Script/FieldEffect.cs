ff1ad1ac2eab395bdaebbbe3ab057282040381a7
RED\Soravit <soravit.viv@gmail.com>
2017-01-22 22:21:11 +0700

    - Field effect class prototype

---
+++ Slime Revenge/Assets/Script/FieldEffect.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class FieldEffect
+{
+    public EffectType effect;
+    public float duration;
+    public string targetUnitType;
+    public string targetStatus;
+
+
+    public enum EffectType
+    {
+        Buff = 1,
+        Debuff = 2,
+        Healing = 3,
+    }
+
+  
+
+
+
+}
---
+++ Slime Revenge/Assets/Script/FieldEffectController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class FieldEffectController : MonoBehaviour
+{
+    //field effect and its start time
+    private Dictionary<FieldEffect, float> m_slimeFieldEffect;
+    private Dictionary<FieldEffect, float> m_enemyFieldEffect;
+    private float m_time;
+    // Use this for initialization
+    void Start()
+    {
+        m_slimeFieldEffect = new Dictionary<FieldEffect, float>();
+        m_enemyFieldEffect = new Dictionary<FieldEffect, float>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_time += Time.deltaTime;
+    }
+
+    public List<FieldEffect> GetSlimeFieldEffect()
+    {
+        List<FieldEffect> fe = new List<FieldEffect>();
+        foreach (KeyValuePair<FieldEffect, float> pair in m_slimeFieldEffect)
+        {
+            //current time < start time + duration
+            if (m_time < (pair.Value + pair.Key.duration))
+            {
+                fe.Add(pair.Key);
+            }
+            else
+            {
+                m_slimeFieldEffect.Remove(pair.Key);
+            }
+        }
+        return fe;
+
+    }
+
+    public List<FieldEffect> GetEnemyFieldEffect()
+    {
+        List<FieldEffect> fe = new List<FieldEffect>();
+        foreach (KeyValuePair<FieldEffect, float> pair in m_enemyFieldEffect)
+        {
+            //current time < start time + duration
+            if (m_time < (pair.Value + pair.Key.duration))
+            {
+                fe.Add(pair.Key);
+            }
+            else
+            {
+                m_enemyFieldEffect.Remove(pair.Key);
+            }
+        }
+        return fe;
+
+    }
+}
