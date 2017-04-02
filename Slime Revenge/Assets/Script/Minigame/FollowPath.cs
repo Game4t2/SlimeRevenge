ac37a5d9e0a2ea59e65de7944662430fa4341b79
pc\korchat <korchat@hotmail.com>
2017-04-02 19:33:19 +0700

    LenAttacked for slimesword &pike +Minigame FollowPath.cs UntestVersion

---
+++ Slime Revenge/Assets/Script/Minigame/FollowPath.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPath : MonoBehaviour {
+
+   public List<GameObject> pathPoint=new List<GameObject>();
+    int index=0;
+  public bool Finished=false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (index == pathPoint.Count)
+            {
+                Finished = true;
+                clearPath();
+            }
+            if (Mathf.Abs(Vector2.Distance(Camera.main.ScreenToWorldPoint(Input.mousePosition), pathPoint[index].transform.position)) < 0.2f)
+            {
+                pathPoint[index].GetComponent<PathPoint>().SetPass(true);
+                index++;
+            }
+
+
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+
+            clearPath();
+        }
+    }
+        public void clearPath()
+    {
+        for(int i=0;i < pathPoint.Count; i++)
+        {
+            pathPoint[i].GetComponent<PathPoint>().SetPass(false);
+        } index = 0;
+    }
+}
+public class PathPoint
+{
+    bool passed=false;
+
+       public void SetPass(bool pass)
+        {
+            passed = pass;
+
+        }
+}
---
+++ Slime Revenge/Assets/Script/Skill/LenAttack.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LenAttack : MonoBehaviour {
+    public float damage = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void SetDamage(float dmg)
+    {
+        damage = dmg;
+
+    }
+
+    void EnemyAttacked(float lenY)
+    {
+     GameObject[] Enemy=  GameObject.FindGameObjectsWithTag("Human");
+        for(int i = 0; i < Enemy.Length; i++)
+        {
+
+            if (Enemy[i].transform.position.y>lenY+0.1f|| Enemy[i].transform.position.y < lenY - 0.1f)
+            {
+                Enemy[i].GetComponent<EnemyUnit>().currentHp -= damage;
+
+            }
+
+        }
+
+    }
+}
